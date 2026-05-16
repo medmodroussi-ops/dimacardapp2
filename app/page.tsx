dd6f85debd0a23f56a1414b4c0fac4ae405912f5
Medmodroussi <100138533+Medmodroussi@users.noreply.github.com>
2026-05-16 16:04:46 +0100

    Premier commit : Initialisation du projet6

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function RootPage() {
   // Redirige automatiquement tout visiteur de la racine (/) vers (/login)
-  redirect('/login');
+  redirect('/dashboard');
 }
